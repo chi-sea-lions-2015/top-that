11b6d1a12197243fe7785fe7f2fda75fb8a72b03
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-17 19:49:12 -0500

    fixed welcome index to allow rspec to run

--- app/controllers/welcome_index_controller.rb
+++ app/controllers/welcome_index_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeIndexController < ApplicationController
 
   def index #research how to take thumbnail of video
-    @video_collection = []
-    PublicArena.where(status: "in_battle").each do |pa|
-      pa.challenger
   end
 end
