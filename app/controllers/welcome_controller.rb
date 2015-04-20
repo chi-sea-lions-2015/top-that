b284843f8d2c94cfaefd82f72aa086d0f0eec52d
Susan Schmidt <schmidtsusanr@gmail.com>
2015-04-20 12:37:21 -0500

    preparing to merge with master

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index #research how to take thumbnail of video
-    @challenger_videos = PublicArena.all_challenger_videos
-    render :json => @challenger_videos
+   if current_user
+     @user = current_user
+   end
+   @video = Video.new
+   @challenger_videos = PublicArena.all_challenger_videos
+   render "index"
   end
 
 
