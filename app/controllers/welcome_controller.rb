93a6b70d6f966c24a7ec189c0d9401f392e190b9
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-19 21:10:27 -0500

    test changing from json to rendering view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index #research how to take thumbnail of video
     @challenger_videos = PublicArena.all_challenger_videos
-    render :json => @challenger_videos
+    # render :json => @challenger_videos
   end
 
 
